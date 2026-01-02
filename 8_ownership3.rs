5a27bf3d8a4a835691a1c19571382ed9048edcb4
Anurag Gupta <anurag6967@gmail.com>
2026-01-02 23:18:17 +0530

    added comments to better undersanding

--- 8_ownership3.rs
+++ 8_ownership3.rs
@@ -1,9 +1,9 @@
 fn main(){
     let s1:String = String::from("hello");
-    let length = calculate_length(s1.clone());
-    println!("The length of '{}' is {}.", s1, length);
+    let length = calculate_length(s1.clone()); // it copy the s1 to the function calculate_length s argument
+    println!("The length of '{}' is {}.", s1, length); //it work because we are not dealing with ownership concept of rust here
 }
 
 fn calculate_length(s: String) -> usize {
-    s.len()
+    s.len() // return the length of the string s
 }
